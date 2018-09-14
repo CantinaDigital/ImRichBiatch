e604544fb7d25cdf4974a0120dd5924b8c27a5df
Mendez <bryan@mndz.io>
2018-09-14 12:32:23 -0500

    imageView changes @ BTN press

--- Im rich, biatch./ViewController.swift
+++ Im rich, biatch./ViewController.swift
@@ -9,7 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let chappelleImageArray = ["Ashy-Larry", "blackDude"]
+    
+    var randomChappeleImage: Int = 0
+    
+    //var randomChappelleSaying: String = "Give it to me, baby!"
+    
+    func randomFaceAndMessage() {
+        
+        randomChappeleImage = Int.random(in: 0 ... 1)
+        
+        viewImage.image = UIImage.init(named: chappelleImageArray[randomChappeleImage])
+        
+    }
+    
+    
+    
+    @IBOutlet weak var viewTitle: UILabel!
+    
+    @IBOutlet weak var viewImage: UIImageView!
+    
+    @IBAction func ranButton(_ sender: UIButton) {
+        randomFaceAndMessage()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
