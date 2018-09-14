3c7bc851671398982467f96d4a1a4fc36002a3ca
Mendez <bryan@mndz.io>
2018-09-14 11:58:39 -0500

    Initial X Commit

---
+++ Im rich, biatch./ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Im rich, biatch.
+//
+//  Created by NASA on 9/14/18.
+//  Copyright © 2018 Cantina Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
